4d255dd71bf5eda6ecf1fd32c01d8263793d1d2a
Fadel <fadel.g.heji@gmail.com>
2022-03-26 21:05:02 -0700

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+ function receivesAFunction(callBackFunc){
+    callBackFunc()
+ }
+ 
+ 
+function returnsANamedFunction(){
+    return function randomFunction(){}
+}
+
+ function returnsAnAnonymousFunction(){
+    return function (){}
+ }
+
